b171c1599ec6d603db16f85903a3500594612606
Daniel H. Fuzari <dhfuzari@gmail.com>
2019-10-12 22:05:40 -0300

    feat(app.js): config app.js tasks - html, css, js and imgs

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -3,9 +3,7 @@ const uglifyCss = require('gulp-uglifycss');
 const concat = require('gulp-concat');
 
 
-gulp.task('deps', ['deps.css', 'deps.fonts',], () => {
-
-});
+gulp.task('deps', ['deps.css', 'deps.fonts']);
 
 gulp.task('deps.css', () => {
     return gulp.src([
@@ -13,11 +11,10 @@ gulp.task('deps.css', () => {
     ])
         .pipe(uglifyCss({ "uglyComments": true }))
         .pipe(concat('deps.min.css'))
-        .pipe(gulp.dest('build/assets/css'))
-
+        .pipe(gulp.dest('build/assets/css'));
 });
 
 gulp.task('deps.fonts', () => {
     return gulp.src(['node_modules/font-awesome/fonts/*.*'])
-        .pipe(gulp.dest('build/assets/fonts'))
+        .pipe(gulp.dest('build/assets/fonts'));
 });
